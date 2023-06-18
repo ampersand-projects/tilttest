3bcf2ee444e4ffeabc3a0b787d7d164ed77b74bd
Anand J <anandj@cs.toronto.edu>
2023-06-17 23:27:21 -0400

    Update tilt

--- main.cpp
+++ main.cpp
@@ -36,24 +36,49 @@ Op _NestedSelect(_sym in, int w)
     auto win_sym = _sym("win", win);
     auto inner_sel = _Select(win_sym, [](_sym e) { return e + _f32(10); });
     auto inner_sel_sym = _sym("inner_sel", inner_sel);
+    auto inner_sel2 = _Select(inner_sel_sym, [](_sym e) {return e + _f32(10); });
+    auto inner_sel2_sym = _sym("inner_sel2", inner_sel2);
     auto sel_op = _op(
         _iter(0, w),
         Params{in},
-        SymTable{{win_sym, win}, {inner_sel_sym, inner_sel}},
+        SymTable{{win_sym, win}, {inner_sel_sym, inner_sel}, {inner_sel2_sym, inner_sel2}},
         _true(),
-        inner_sel_sym);
+        inner_sel2_sym);
     return sel_op;
+}
+
+Expr _Sum(_sym win)
+{
+    auto acc = [](Expr s, Expr st, Expr et, Expr d) { return _add(s, d); };
+    return _red(win, _f32(0), acc);
+}
+
+Op _WindowSum(_sym in, int64_t w)
+{
+    auto window = in[_win(-w, 0)];
+    auto window_sym = _sym("win", window);
+    auto sum = _Sum(window_sym);
+    auto sum_sym = _sym("win_sum", sum);
+    auto wc_op = _op(
+        _iter(0, w),
+        Params{ in },
+        SymTable{ {window_sym, window}, {sum_sym, sum} },
+        _true(),
+        sum_sym);
+    return wc_op;
 }
 
 int main(int argc, char* argv[])
 {
     int dlen = (argc > 1) ? atoi(argv[1]) : 30;
     int len = (argc > 2) ? atoi(argv[2]) : 10;
+    dur_t dur = (argc > 3) ? atoi(argv[3]) : 2;
 
     // input stream
-    auto in_sym = _sym("in", tilt::Type(types::FLOAT32, _iter(0, -1)));
+    auto in_sym = _sym("in", tilt::Type(types::FLOAT32, _iter(0, dur)));
 
-    auto query_op = _NestedSelect(in_sym, 10);
+    auto query_op = _WindowSum(in_sym, 10);
+    //auto query_op = _NestedSelect(in_sym, 10);
     //auto query_op = _Select(in_sym, [](_sym e) { return e + _f32(10); });
     auto query_op_sym = _sym("query", query_op);
     cout << endl << "TiLT IR:" << endl;
@@ -73,12 +98,11 @@ int main(int argc, char* argv[])
 
     auto loop_addr = (region_t* (*)(ts_t, ts_t, region_t*, region_t*)) jit->Lookup(loop->get_name());
 
-    uint32_t dur = 1;
     auto buf_size = get_buf_size(dlen);
 
     auto in_data = new float[dlen];
     region_t in_reg;
-    init_region(&in_reg, 0, buf_size, reinterpret_cast<char*>(in_data));
+    init_region(&in_reg, 0, dur, buf_size, reinterpret_cast<char*>(in_data));
     for (int i = 0; i < dlen; i++) {
         auto t = dur * i;
         commit_data(&in_reg, t);
